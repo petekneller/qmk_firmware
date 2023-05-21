2de281c1bc0c11bbda7a79f20ce971721e44eaa8
Pete Kneller <pommypete@gmail.com>
2023-05-21 22:50:58 +0100

    Adds Caps Word to try it out

--- keyboards/crkbd/keymaps/petekneller/config.h
+++ keyboards/crkbd/keymaps/petekneller/config.h
@@ -1,4 +1,8 @@
 #pragma once
 
+// Mod-taps
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
+
+// Caps word
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
---
+++ keyboards/crkbd/keymaps/petekneller/rules.mk
@@ -0,0 +1,4 @@
+# Caps word
+# ---------
+COMMAND_ENABLE = no # Command overlaps both shift key activation
+CAPS_WORD_ENABLE = yes
